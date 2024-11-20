7df8bd22d550e1974ddee0fe2470960b7cb3f263
Nykyforuk Vasyl <nikiforuk.vasya@gmail.com>
2024-11-20 02:40:10 +0200

    add post, delete

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -3,6 +3,8 @@ import { Router } from 'express';
 import {
   getContactsController,
   getContactByIdController,
+  createContactController,
+  deleteContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper';
 
@@ -10,5 +12,9 @@ const router = Router();
 
 router.get('/contacts', ctrlWrapper(getContactsController));
 router.get('/contacts/:/contactId', ctrlWrapper(getContactByIdController));
+
+router.post('/contacts', ctrlWrapper(createContactController));
+
+router.delete('/contacts/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
